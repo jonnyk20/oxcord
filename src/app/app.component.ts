8c49a75a497b3569b57342cb7c8f16c034493683
Jonathan Kalambay <jonnyk20@gmail.com>
2017-06-29 21:53:40 +0200

    Yah

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+//COMPONENT TS
+
 import { Component } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
 import { AngularFireAuthModule,AngularFireAuth} from 'angularfire2/auth';
@@ -12,44 +14,17 @@ import { AngularFireAuthModule,AngularFireAuth} from 'angularfire2/auth';
 
 export class AppComponent {
     title = 'Oxcord';
-/*
-    items: FirebaseListObservable<any[]>;
-  constructor(db: AngularFireDatabase) {
-    this.items = db.list('/items');
-  }
-  */
 
-  Songz: FirebaseObjectObservable<any>;
+
+  songs: FirebaseObjectObservable<any>;
    constructor(db: AngularFireDatabase) {
-    this.Songz = db.object('/Songz');
+    this.songs = db.object('/songs');
   }
-/*
-save(newName: string) {
-    this.testobject.update({ name: newName });
-  }
-*/
-
-
-
-  songs = [
   
-  {title: "Song", artist: "Artist", likes: 1},
-  {title: "Chanson", artist: "Artiste", likes: 3},
-  {title: "ABC", artist: "OneTwoThree", likes: 2},
-  {title: "Trash", artist: "Meek Mill", likes: 0}
-  
-  ];
-  addLike(input){
-  this.songs[input].likes +=1;
+  addLike(index){
+this.songs[index].update({likes: this.songs[index] + 1 });
+
 } 
-removeLike(input){
-  this.songs[input].likes -=1;
-} 
-args="Me";
-
-
-
-
 
 
 
